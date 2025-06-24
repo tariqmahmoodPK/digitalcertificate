f1408905374a66c2a0ddaf61e06c11222e4e68c5
Tariq Mahmood <75470442+tariqmahmoodPK@users.noreply.github.com>
2025-06-24 23:29:01 +0500

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return 'Hello from Cloud Run!'
+    return 'Hello from Cloud Run - digitalcertificate service!'
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8080)

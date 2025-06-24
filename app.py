0450d8e20435a4fb796109bfc46d7c43f45f35fa
Tariq Mahmood <75470442+tariqmahmoodPK@users.noreply.github.com>
2025-06-25 00:36:07 +0500

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,16 @@
-from flask import Flask
+from flask import Flask, Response
+import os
+
 app = Flask(__name__)
 
 @app.route('/')
-def home():
-    return 'Hello from Cloud Run - digitalcertificate service!'
+def show_certificate():
+    try:
+        with open('digital-certificate.txt', 'r') as cert_file:
+            content = cert_file.read()
+        return Response(content, mimetype='text/plain')
+    except FileNotFoundError:
+        return "Certificate file not found.", 404
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8080)
